7bc4fc935a0495fe437d8d396b14940547f403c7
Emmanuel Ochonma <ofine360@gmail.com>
2023-08-08 19:42:50 -0700

    my first

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,39 +1,48 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-/* Definition of singly linked list node structure */
-typedef struct listint_s
-{
+typedef struct listint_s {
     int n;
     struct listint_s *next;
 } listint_t;
 
-/* Function to insert a number into a sorted singly linked list */
-listint_t *insert_node(listint_t **head, int number)
-{
+listint_t *insert_node(listint_t **head, int number) {
     listint_t *new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-    {
-        return NULL;  /* Return NULL if memory allocation fails */
+    if (new_node == NULL) {
+        return NULL; // Memory allocation failed
     }
-
     new_node->n = number;
+    new_node->next = NULL;
 
-    if (*head == NULL || number < (*head)->n)
-    {
+    if (*head == NULL || (*head)->n >= number) {
         new_node->next = *head;
         *head = new_node;
-    }
-    else
-    {
+    } else {
         listint_t *current = *head;
-        while (current->next != NULL && number > current->next->n)
-        {
+        while (current->next != NULL && current->next->n < number) {
             current = current->next;
         }
         new_node->next = current->next;
         current->next = new_node;
     }
 
-    return new_node;  /* Return the address of the new node */
+    return new_node;
+}
+
+int main() {
+    listint_t *head = NULL;
+    insert_node(&head, 5);
+    insert_node(&head, 10);
+    insert_node(&head, 7);
+    insert_node(&head, 3);
+
+    // Print the linked list to verify insertion
+    listint_t *current = head;
+    while (current != NULL) {
+        printf("%d ", current->n);
+        current = current->next;
+    }
+
+    return 0;
 }
 
