857c8be091d5699796a6b7a63d900989c079f25d
Emmanuel Ochonma <ofine360@gmail.com>
2023-08-11 14:34:19 -0700

    my second

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 a = 10
 b = 5
-
 from calculator_1 import add, subtract, multiply, divide
-
 sum_result = add(a, b)
 diff_result = subtract(a, b)
 prod_result = multiply(a, b)
 div_result = divide(a, b)
-
-print(f"{a} + {b} = {sum_result}")
-print(f"{a} - {b} = {diff_result}")
-print(f"{a} * {b} = {prod_result}")
-print(f"{a} / {b} = {div_result}")
-
+print("{} + {} = {}".format(a, b, sum_result))
+print("{} - {} = {}".format(a, b, diff_result))
+print("{} * {} = {}".format(a, b, prod_result))
+print("{} / {} = {}".format(a, b, div_result))
