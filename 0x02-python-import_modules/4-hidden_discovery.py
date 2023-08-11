4d0d24bc6c1248fa4b8ce1b30e730872b4f562f1
Emmanuel Ochonma <ofine360@gmail.com>
2023-08-11 15:11:33 -0700

    my fifth

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-import importlib.util
 
 if __name__ == "__main__":
-    module_name = "hidden_4"
+    import hidden_4
     
-    spec = importlib.util.spec_from_file_location(module_name, "path/to/hidden_4.pyc")
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    
-    names = [name for name in dir(module) if not name.startswith("__")]
-    sorted_names = sorted(names)
-    
-    for name in sorted_names:
-        print(name)
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
+            print(name)
