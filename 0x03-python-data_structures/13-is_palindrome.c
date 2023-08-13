6c1e68b9b837761e14bd83230121877567f3ce0e
Emmanuel Ochonma <ofine360@gmail.com>
2023-08-13 06:27:52 -0700

    my commit

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,32 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Pointer to the beginning of the linked list.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
  */
-typedef struct listint_s
+int is_palindrome(listint_t **custom_head)
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
-
-/**
- * is_palindrome - Checks if a singly linked list is a palindrome
- * @head: Pointer to a pointer to the head of the list
- *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- * An empty list is considered a palindrome
- */
-int is_palindrome(listint_t **head)
-{
-    listint_t *fast = *head;
-    listint_t *slow = *head;
+    listint_t *slow = *custom_head;
+    listint_t *fast = *custom_head;
     listint_t *prev = NULL;
     listint_t *temp;
+
+    if (*custom_head == NULL || (*custom_head)->next == NULL)
+        return (1);
 
     while (fast != NULL && fast->next != NULL)
     {
@@ -40,45 +27,13 @@ int is_palindrome(listint_t **head)
     if (fast != NULL)
         slow = slow->next;
 
-    while (slow != NULL)
+    while (prev != NULL && slow != NULL)
     {
-        if (slow->n != prev->n)
-            return 0;
-        slow = slow->next;
+        if (prev->n != slow->n)
+            return (0);
         prev = prev->next;
+        slow = slow->next;
     }
 
-    return 1;
+    return (1);
 }
-
-int main(void)
-{
-    listint_t *head = NULL;
-    listint_t *node1 = malloc(sizeof(listint_t));
-    listint_t *node2 = malloc(sizeof(listint_t));
-    listint_t *node3 = malloc(sizeof(listint_t));
-    listint_t *node4 = malloc(sizeof(listint_t));
-    listint_t *node5 = malloc(sizeof(listint_t));
-
-    node1->n = 1;
-    node2->n = 2;
-    node3->n = 3;
-    node4->n = 2;
-    node5->n = 1;
-
-    node1->next = node2;
-    node2->next = node3;
-    node3->next = node4;
-    node4->next = node5;
-    node5->next = NULL;
-
-    head = node1;
-
-    if (is_palindrome(&head))
-        printf("Linked list is a palindrome.\n");
-    else
-        printf("Linked list is not a palindrome.\n");
-
-    return 0;
-}
-
