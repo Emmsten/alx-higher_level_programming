6f4a6bc9889afce75a366217ed83e8e0386aa179
Emmanuel Ochonma <ofine360@gmail.com>
2023-08-13 05:37:29 -0700

    this is the day

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Get the first two elements of each tuple or 0 if the tuple is smaller
+    a1, a2 = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    b1, b2 = tuple_b + (0, 0)[:2 - len(tuple_b)]
+
+    # Calculate the sum of corresponding elements and return a new tuple
+    return (a1 + b1, a2 + b2)
