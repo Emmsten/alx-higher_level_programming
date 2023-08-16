74f37ac8f7fee7517cbe28ae7bbd2f47a199a8d1
Emmanuel Ochonma <ofine360@gmail.com>
2023-08-16 15:15:15 -0700

    my commit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(custom_dictionary):
-    return {my_key: custom_value * 2 for my_key, my_value imy_dictionary.items()}
+def multiply_by_2(my_dictionary):
+    return {my_key: my_value * 2 for my_key, my_value in my_dictionary.items()}
