f9e429f75abb439eb31be061eca34bce3898a65d
Emmanuel Ochonma <ofine360@gmail.com>
2023-08-16 15:19:42 -0700

    in addy

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(custom_dictionary):
-    [print("{}: {}".format(i, custom_dictionary[i])) for i in sorted(custom_dictionary)]
+def print_sorted_dictionary(_dictionary):
+    [print("{}: {}".format(i, my_dictionary[i])) for i in sorted(my_dictionary)]
