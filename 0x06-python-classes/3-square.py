7c7d8a7ec5df843fc98823cdc2d92baeff464b91
Emmanuel Ochonma <ofine360@gmail.com>
2024-01-23 11:04:59 -0800

    object

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    Class Square that defines a square.
+
+    Attributes:
+        __size (int): Private instance attribute for the size of the square.
+    """
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
