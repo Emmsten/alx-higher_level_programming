f9d34a899385ec9d097caac410b6bcff52387631
Emmanuel Ochonma <ofine360@gmail.com>
2024-01-29 15:25:04 -0800

    almost

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -17,75 +17,85 @@ This module contains a class Rectangle
 
 
 class Rectangle:
-    """Defines a rectangle"""
+    """This is an class Rectangle with instance attribute height and width"""
 
     number_of_instances = 0
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        """Initialization method"""
+        """
+        initializes height and width of the rectangle
+        upon creation of an instance using property
+        getters and setters
+
+        >>> my_rectangle = Rectangle(2, '4')
+        Traceback (most recent call last):
+            ...
+        TypeError: height must be an integer
+
+
+        >>> my_rectangle = Rectangle(0, 0)
+        >>> my_rectangle.width = 10
+        >>> my_rectangle.height = -3
+        Traceback (most recent call last):
+            ...
+        ValueError: height must be >= 0
+        """
         self.width = width
         self.height = height
         type(self).number_of_instances += 1
 
     @property
     def width(self):
-        """Property getter for width"""
         return self.__width
 
     @width.setter
-    def width(self, value):
-        """Property setter for width"""
-        if not isinstance(value, int):
+    def width(self, width):
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
-        """Property getter for height"""
         return self.__height
 
     @height.setter
-    def height(self, value):
-        """Property setter for height"""
-        if not isinstance(value, int):
+    def height(self, height):
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__height = height
 
     def area(self):
-        """Calculates the area of the rectangle"""
         return self.width * self.height
 
     def perimeter(self):
-        """Calculates the perimeter of the rectangle"""
-        if self.width == 0 or self.height == 0:
+        if self.height == 0 or self.width == 0:
             return 0
         return 2 * (self.width + self.height)
 
     def __str__(self):
-        """Returns a string representation of the rectangle"""
         if self.width == 0 or self.height == 0:
             return ""
-        return str(self.print_symbol) * self.width + '\n' * (self.height - 1) + str(self.print_symbol) * self.width
+        shape_rep_array = []
+        for height in range(self.height):
+            shape_rep_array.append(self.print_symbol * self.width)
+            shape_rep_array.append("\n")
+        shape_rep_array.pop()
+        return "".join(shape_rep_array)
 
     def __repr__(self):
-        """Returns a string representation of the rectangle
-        that can be used to create a new instance"""
         return "Rectangle({}, {})".format(self.width, self.height)
 
     def __del__(self):
-        """Destructor method"""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
 
-if __name__ == "__main__":
-    # Example usage
-    r1 = Rectangle(3, 4)
-    print(r1)
-    print(repr(r1))
-    del r1
-    print(Rectangle.number_of_instances)
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("Bye re
