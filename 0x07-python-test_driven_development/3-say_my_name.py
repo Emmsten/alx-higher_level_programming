6525b2e34e900af58d680a665e8dde0e4591a8a9
Emmanuel Ochonma <ofine360@gmail.com>
2024-01-30 15:58:47 -0800

    time to fill

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,48 +1,28 @@
 #!/usr/bin/python3
+
 """
-    This module accepts a matrix and a divisor and,
-    returns the result of division of each element in a list
-    in the corresponding list index
+    This module takes in two string arguments and prints formatted
+    output.
 
     Args:
-        matrix (list: int): This must be a list of list of integers or float
-        div (int or float): second argument to function
+        first_name (str): first parameter
+        last_name (str): second parameter
+    Raises:
+        TypeError: if either of the arguments is not a string
 """
 
 
-def matrix_divided(matrix, div):
-    """
-        Returns a list of list of floating point numbers
-    """
+def say_my_name(first_name, last_name=""):
+    """Prints formatted output of argument"""
 
-    err_row_size = "Each row of the matrix must have the same size"
-    type_err = "matrix must be a matrix (list of lists) of integers/floats"
-    err_zero_division = "division by zero"
+    first_name_err = "first_name must be a string"
+    last_name_err = "last_name must be a string"
 
-    if (not matrix):
-        return (None)
+    if (not isinstance(first_name, str)):
+        raise TypeError(first_name_err)
 
-    if (not isinstance(matrix, list)):
-        raise TypeError(type_err)
+    elif (not isinstance(last_name, str)):
+        raise TypeError(last_name_err)
 
-    if (type(div) in [int, float]):
-        if (div == 0):
-            raise ZeroDivisionError(err_zero_division)
     else:
-        raise TypeError("div must be a number")
-
-    i = 0
-    while (i < len(matrix) - 1):
-        if (len(matrix[i]) != len(matrix[i + 1])):
-            raise TypeError(err_row_size)
-        i += 1
-
-    for mat in matrix:
-        if (not isinstance(mat, list)):
-            raise TypeError(type_err)
-        for ele in mat:
-            if type(ele) not in [int, float]:
-                raise TypeError(type_err)
-
-    new = [[round(ele/div, 2) for ele in mat] for mat in matrix]
-    return (new)
+        print("My name is {} {}".format(first_name, last_name))
