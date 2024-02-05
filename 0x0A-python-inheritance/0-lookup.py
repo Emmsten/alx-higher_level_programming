3109d52667ed99d442f7e066980db552c4e86e65
Emmanuel Ochonma <ofine360@gmail.com>
2024-02-05 15:14:28 -0800

    correction

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,4 +7,3 @@ contains the lookup function
 def lookup(obj):
     """Returns a list of available attributes and methods of an object."""
     return [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
-
